c0199d9df59abb7de5379641d1ed3d4c67a7b0cb
AlexAndHisScripts <40502404+AlexAndHisScripts@users.noreply.github.com>
2019-10-20 08:57:01 +0000

    Experimenting with running class Api

--- CLIENT/Gui-App/Gui-App/Program.cs
+++ CLIENT/Gui-App/Gui-App/Program.cs
@@ -16,6 +16,7 @@ namespace Gui_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Api());
             Application.Run(new Form1());
         }
     }
